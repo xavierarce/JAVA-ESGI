29bc389be5dc4e77672194800649b4ac47c272e3
Xavier Arce <xavierarce54321@gmail.com>
2025-06-12 14:20:52 +0200

    Created MathUtils

---
+++ Fraction/MathUtils.java
@@ -0,0 +1,16 @@
+package Fraction;
+
+public class MathUtils {
+
+  public static int pgcd(int a, int b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+}
