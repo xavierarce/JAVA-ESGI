f38732ba5c0fdf3bde55f57f4afa3db916fc6837
Xavier Arce <xavierarce54321@gmail.com>
2025-06-12 11:11:11 +0200

    Refactor Player class to improve structure and add takeDamage and isAlive methods

--- Hello Player/Player.java
+++ Hello Player/Player.java
@@ -1,14 +1,32 @@
 public class Player {
- private final String name;
- public Player(String name) {
- this.name = name;
- }
- public void greet() {
- System.out.println("Bonjour, je suis "
-+ name + " !");
- }
- public static void main(String[] args) {
- Player p = new Player("Alice");
- p.greet();
- }
+  private final String name;
+  private int hp;
+
+  public Player(String name) {
+    this.name = name;
+    this.hp = 100;
+  }
+
+  public void greet() {
+    System.out.println("Bonjour, je suis " + name + " !");
+  }
+
+  public void takeDamage(int amount) {
+    hp -= amount;
+    if (hp < 0) {
+      hp = 0;
+    }
+    System.out.println(name + " a subi " + amount + " points de dégâts. Points de vie restants : " + hp);
+  }
+
+  public boolean isAlive() {
+    return hp > 0;
+  }
+
+  public static void main(String[] args) {
+    Player p = new Player("Xavier");
+    p.greet();
+    p.takeDamage(15);
+    System.out.println("Le joueur est-il en vie ? " + p.isAlive());
+  }
 }
