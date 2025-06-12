b1038ee6550eb52332e75f7d1fc861f51119c83f
Xavier Arce <xavierarce54321@gmail.com>
2025-06-12 15:31:18 +0200

    Added name to weapon

--- Hello Player/Weapon.java
+++ Hello Player/Weapon.java
@@ -1,16 +1,22 @@
 import java.util.Random;
 
 public class Weapon {
+  private String name;
   private int minDamage;
   private int maxDamage;
   private Random rand = new Random();
 
-  public Weapon(int minDamage, int maxDamage) {
+  public Weapon(String name, int minDamage, int maxDamage) {
+    this.name = name;
     this.minDamage = minDamage;
     this.maxDamage = maxDamage;
   }
 
   public int rollDamage() {
     return rand.nextInt(maxDamage - minDamage + 1) + minDamage;
+  }
+
+  public String getName() {
+    return name;
   }
 }
