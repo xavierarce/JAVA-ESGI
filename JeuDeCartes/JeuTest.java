0da35af97f66cdd246a4c7bf3d91da42c1cb527e
Xavier Arce <xavierarce54321@gmail.com>
2025-06-13 11:56:01 +0200

    Extracted Jeu de cartes logic

---
+++ JeuDeCartes/JeuTest.java
@@ -0,0 +1,29 @@
+package JeuDeCartes;
+
+public class JeuTest {
+  public static void testerCartes() {
+    Carte[] main = new Carte[3];
+    main[0] = new CarteChiffre("Cœur", 7);
+    main[1] = new CarteSpecial("Joker", "Change la couleur");
+    main[2] = new CarteChiffre("Pique", 1);
+
+    for (Carte c : main) {
+      c.jouer();
+      System.out.println(c);
+    }
+
+    // Test equals
+    Carte c1 = new CarteChiffre("Cœur", 7);
+    Carte c2 = new CarteChiffre("Cœur", 7);
+    Carte c3 = new CarteChiffre("Pique", 1);
+    Carte c4 = new CarteSpecial("Joker", "Change la couleur");
+    Carte c5 = new CarteSpecial("Joker", "Change la couleur");
+    Carte c6 = new CarteSpecial("Joker", "Bloque le joueur");
+
+    System.out.println("\nTesting equals:");
+    System.out.println("c1 equals c2? " + c1.equals(c2));
+    System.out.println("c1 equals c3? " + c1.equals(c3));
+    System.out.println("c4 equals c5? " + c4.equals(c5));
+    System.out.println("c4 equals c6? " + c4.equals(c6));
+  }
+}
